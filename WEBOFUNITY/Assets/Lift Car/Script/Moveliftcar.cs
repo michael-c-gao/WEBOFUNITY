3950c161040dcc9a261ef378cafa80343c0edd9a
michael-c-gao <michaelgao16@gmail.com>
2021-05-12 00:19:03 -0700

    commit

--- WEBOFUNITY/Assets/Lift Car/Script/Moveliftcar.cs
+++ WEBOFUNITY/Assets/Lift Car/Script/Moveliftcar.cs
@@ -1,32 +1,37 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
-public class Moveliftcar : MonoBehaviour {
-
-
-
-		public Transform target;
-		public float speed = 0.0f;
-		public KeyCode Key1;
-		public KeyCode Key2;
-		public Vector3 forwardPos;
-		public Vector3 rearPos;
+public class Moveliftcar : MonoBehaviour
+{
+	public GameObject Player;
+	private Vector3 _angles;
 
 
-
-		void Update () {
-
-
-			if (Input.GetKey(Key1))
-
-			{
+	public Transform target;
+	public float speed = 0.0f;
+	
+	public Vector3 forwardPos;
+	public Vector3 rearPos;
 
 
 
-				target.transform.localPosition = Vector3.MoveTowards(target.transform.localPosition, forwardPos, speed  * Time.deltaTime);
+	void Update()
+	{
+
+
+
+
+			Vector3 d = Player.transform.position - transform.position;
+			if ((d.x > 1) && (d.y > 1) && (d.z > 1)){ 
+
+
+
+			target.transform.localPosition = Vector3.MoveTowards(target.transform.localPosition, forwardPos, speed  * Time.deltaTime);
 
 			}
-			if (Input.GetKey(Key2))
+			if ((d.x < 1) && (d.y < 1) && (d.z < 1))
 			{
 				target.transform.localPosition = Vector3.MoveTowards (target.transform.localPosition, rearPos, speed * Time.deltaTime);
 			}
--- WEBOFUNITY/Assets/Scripts/PlayerMovement.cs
+++ WEBOFUNITY/Assets/Scripts/PlayerMovement.cs
@@ -29,7 +29,7 @@ public class PlayerMovement : MonoBehaviour
     public float maxSlopeAngle = 35f;
 
     //Crouch & Slide
-    private Vector3 crouchScale = new Vector3(1, 0.5f, 1);
+     private Vector3 crouchScale = new Vector3(1, 0.5f, 1);
     private Vector3 playerScale;
     public float slideForce = 400;
     public float slideCounterMovement = 0.2f;
@@ -87,32 +87,15 @@ public class PlayerMovement : MonoBehaviour
         x = Input.GetAxisRaw("Horizontal");
         y = Input.GetAxisRaw("Vertical");
         jumping = Input.GetButton("Jump");
-        crouching = Input.GetKey(KeyCode.LeftControl);
+        
 
-        //Crouching
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-            StartCrouch();
-        if (Input.GetKeyUp(KeyCode.LeftControl))
-            StopCrouch();
+
     }
 
-    private void StartCrouch()
-    {
-        transform.localScale = crouchScale;
-        transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
-        if (rb.velocity.magnitude > 0.5f)
-        {
-            if (grounded)
-            {
-                rb.AddForce(orientation.transform.forward * slideForce);
-            }
-        }
-    }
 
     private void StopCrouch()
     {
-        transform.localScale = playerScale;
-        transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+        
     }
 
     private void Movement()
@@ -133,12 +116,6 @@ public class PlayerMovement : MonoBehaviour
         //Set max speed
         float maxSpeed = this.maxSpeed;
 
-        //If sliding down a ramp, add force down so player stays grounded and also builds speed
-        if (crouching && grounded && readyToJump)
-        {
-            rb.AddForce(Vector3.down * Time.deltaTime * 3000);
-            return;
-        }
 
         //If speed is larger than maxspeed, cancel out the input so you don't go over max speed
         if (x > 0 && xMag > maxSpeed) x = 0;
@@ -192,39 +169,15 @@ public class PlayerMovement : MonoBehaviour
         readyToJump = true;
     }
 
-    private float desiredX;
-    /*
-     * Now instead of look we are using a third-person camera:
-     * https://www.youtube.com/watch?v=4HpC--2iowE
-    */
-    /*private void Look()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
-        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
-
-        //Find current look rotation
-        Vector3 rot = playerCam.transform.localRotation.eulerAngles;
-        desiredX = rot.y + mouseX;
-
-        //Rotate, and also make sure we dont over- or under-rotate.
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        //Perform the rotations
-        playerCam.transform.localRotation = Quaternion.Euler(xRotation, desiredX, 0);
-        orientation.transform.localRotation = Quaternion.Euler(0, desiredX, 0);
-    }*/
+  
+   
+   
 
     private void CounterMovement(float x, float y, Vector2 mag)
     {
         if (!grounded || jumping) return;
 
-        //Slow down sliding
-        if (crouching)
-        {
-    rb.AddForce(moveSpeed * speedScalar * Time.deltaTime * -rb.velocity.normalized * slideCounterMovement);
-    return;
-}
+       
 
 //Counter movement
 if (Math.Abs(mag.x) > threshold && Math.Abs(x) < 0.05f || (mag.x < -threshold && x > 0) || (mag.x > threshold && x < 0))
@@ -236,20 +189,16 @@ if (Math.Abs(mag.y) > threshold && Math.Abs(y) < 0.05f || (mag.y < -threshold &&
     rb.AddForce(moveSpeed * speedScalar * orientation.transform.forward * Time.deltaTime * -mag.y * counterMovement);
 }
 
-//Limit diagonal running. This will also cause a full stop if sliding fast and un-crouching, so not optimal.
+
 if (Mathf.Sqrt((Mathf.Pow(rb.velocity.x, 2) + Mathf.Pow(rb.velocity.z, 2))) > maxSpeed)
 {
-    float fallspeed = rb.velocity.y;
+    float fallspeed = rb.velocity.y*3;
     Vector3 n = rb.velocity.normalized * maxSpeed;
     rb.velocity = new Vector3(n.x, fallspeed, n.z);
 }
     }
 
-    /// <summary>
-    /// Find the velocity relative to where the player is looking
-    /// Useful for vectors calculations regarding movement and limiting movement
-    /// </summary>
-    /// <returns></returns>
+    
     public Vector2 FindVelRelativeToLook()
 {
     float lookAngle = orientation.transform.eulerAngles.y;
@@ -273,9 +222,7 @@ private bool IsFloor(Vector3 v)
 
 private bool cancellingGrounded;
 
-/// <summary>
-/// Handle ground detection
-/// </summary>
+
 private void OnCollisionStay(Collision other)
 {
     //Make sure we are only checking for walkable layers
