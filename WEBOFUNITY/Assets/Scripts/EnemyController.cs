693007311f1832f5c59df366b1ae795a9242f8c6
dhan2023 <dhan@uoregon.edu>
2021-05-11 20:07:48 -0700

    Added enemy sample + incomplete script for enemy aggro

---
+++ WEBOFUNITY/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float lookRadius = 10f;
+    public Transform Player;
+
+    Transform target;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    // Source of reference: https://youtu.be/xppompv1DBg
+    void Start()
+    {
+        target = Player;
+        print(target.position);
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if(distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+            
+            if(distance <= agent.stoppingDistance)
+            {
+                //Face player
+                FaceTarget();
+                //Attack player
+            }
+        }
+
+    }
+
+    void FaceTarget()
+    {
+
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    
+    }
+
+    void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
