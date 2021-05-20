5bc83471c67586bdef4c66db4bdd4cf9f7ed5d1e
buddymcdoo <simonward3000@gmail.com>
2021-05-20 15:21:35 -0700

    Changed the spiderweb color/texture. Added beatle and mosquito assets (in the ImportedAssets folder).

---
+++ WEBOFUNITY/Assets/ImportedAssets/Monster Beetle/Examples/Scripts/TargetSetter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Monster_Beetle.Scripts;
+using UnityEngine;
+
+public class TargetSetter : MonoBehaviour
+{
+
+    private Camera _camera;
+
+    private BeetleController _beetleController;
+
+    public GameObject Pointer;
+
+    private
+
+    void Start()
+    {
+        _camera = FindObjectOfType<Camera>();
+        _beetleController = FindObjectOfType<BeetleController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+            var raycasts = Physics.RaycastAll(ray).ToArray();
+
+            var hit = raycasts.FirstOrDefault(x => x.transform.name.Equals("Plane"));
+
+            Pointer.transform.position = hit.point;
+        }
+    }
+}
