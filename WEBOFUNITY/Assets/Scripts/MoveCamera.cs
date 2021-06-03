6fd4a72602cec5d11f1548dfae6b55e9060de6ca
buddymcdoo <simonward3000@gmail.com>
2021-06-02 22:05:46 -0700

    Implementing background noise.

--- WEBOFUNITY/Assets/Scripts/MoveCamera.cs
+++ WEBOFUNITY/Assets/Scripts/MoveCamera.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 public class MoveCamera : MonoBehaviour
 {
 
+    public AudioClip backgroundAmbienceSound;
+    private AudioSource backgroundAmbienceAudioSource;
+    
     public Transform player;
 
+    void Start()
+    {
+        backgroundAmbienceAudioSource = GetComponent<AudioSource>();
+    }
 
     void Update()
     {
-
+        if (!backgroundAmbienceAudioSource.isPlaying)
+        {
+            backgroundAmbienceAudioSource.volume = 0.1f;
+            backgroundAmbienceAudioSource.clip = backgroundAmbienceSound;
+            backgroundAmbienceAudioSource.Play();
+        }
     }
 }
