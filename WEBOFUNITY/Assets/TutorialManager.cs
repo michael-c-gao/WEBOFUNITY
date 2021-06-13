dee1484964057b2975f5e65608d23299d76168d1
michael-c-gao <michaelgao16@gmail.com>
2021-06-12 18:13:15 -0700

    finished tutorial

--- WEBOFUNITY/Assets/TutorialManager.cs
+++ WEBOFUNITY/Assets/TutorialManager.cs
@@ -6,46 +6,37 @@ public class TutorialManager : MonoBehaviour
 {
     public GameObject[] popUps;
     private int popUpIndex = 0;
+    
 
 
     void Update()
     {
-        for (int i = 0; i < popUps.Length; i++){
-            if (i == popUpIndex){
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            
+            if (i == popUpIndex)
+            {
                 popUps[i].SetActive(true);
             }else{
                 popUps[i].SetActive(false);
             }
+            if (PauseMenu.isPaused)
+            {
+                popUps[i].SetActive(false);
+            }
+            
+        }
+        
+
+        if (popUpIndex>= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                popUpIndex++;
+            }
         }
 
-        if (popUpIndex == 0)
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                popUpIndex++;
-            }
-        }
-        else if (popUpIndex == 1)
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                popUpIndex++;
-            }
-        }
-        else if (popUpIndex == 2)
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                popUpIndex++;
-            }
-        }
-        else
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                popUpIndex++;
-            }
-        }
+
 
         }
 
